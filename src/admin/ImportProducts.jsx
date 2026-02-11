73a7b3e3e4d7ff0d0a266d001fbd210492497494
Md.Shajin Khan <mdsazinur90@yahoo.com>
2026-02-11 15:31:19 +0600

    update api

--- src/admin/ImportProducts.jsx
+++ src/admin/ImportProducts.jsx
@@ -12,7 +12,7 @@ export default function ImportProducts() {
 
     try {
       await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/admin/import-products`,
+        `${import.meta.env.VITE_API_URL}/admin/import-products`,
         formData,
         {
           headers: {
